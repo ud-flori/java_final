55521ded9cb97a12130d64d31bcc6cb5341cf527
ud-flori <flori.zs14@gmail.com>
2021-11-21 12:24:51 +0100

    Initialization and Spring Shell implementation

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/presentation/cli/CliApplication.java
@@ -0,0 +1,17 @@
+package com.epam.training.ticketservice.presentation.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com.epam.training.ticketservice")
+//@EnableJpaRepositories(basePackages = "com.epam.training.ticketservice.dao.repository")
+//@EntityScan(basePackages = "com.epam.training.ticketservice.dao.repository.entity")
+public class CliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliApplication.class, args);
+    }
+
+}
---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/presentation/cli/TicketServicePromptProvider.java
@@ -0,0 +1,13 @@
+package com.epam.training.ticketservice.presentation.cli;
+
+import org.jline.utils.AttributedString;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TicketServicePromptProvider implements PromptProvider {
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>");
+    }
+}
